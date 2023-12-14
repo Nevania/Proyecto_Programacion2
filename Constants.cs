466897ec110e7abbe62397c94e7e0d4fda28d30a
gaspar <gaspar@pizzati.com>
2023-12-14 16:27:24 -0600

    Configuracion sqlite, dataItem, synk

---
+++ Constants.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Proyecto_Programacion2
+{
+    internal class Constants
+    {
+        public const String DatabaseFilename = "TodoSqlite.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite | SQLite.SQLiteOpenFlags.Create | SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static String DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿
+
+using SQLite;
+
+namespace Proyecto_Programacion2.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Notes { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
