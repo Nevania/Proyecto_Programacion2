08292818a950946a5d877c0bb0c6571b520e4d4b
Nevania <140127237+Nevania@users.noreply.github.com>
2023-12-14 22:40:48 +0100

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto_Programacion2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
